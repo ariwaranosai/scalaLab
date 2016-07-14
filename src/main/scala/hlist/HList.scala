280403d698bb2dbce08de7201d015fee51ea787f
ariwaranosai <nkssai@outlook.com>
2016-07-15 00:37:00 +0800

    HList

--- src/main/scala/hlist/HList.scala
+++ src/main/scala/hlist/HList.scala
@@ -1,4 +1,4 @@
-package naturaltransformation
+package hlist
 
 /**
   * Created by sai on 2016/7/13.
@@ -57,35 +57,25 @@ object HList {
   type Reverse[A <: HList] = A#Foldl[HList, AppHCons.type, HNil.type ]
 
   trait ~>[F[_], G[_]] {
-    type Apply[X] = G[X]
+    type Apply1[X] = G[X]
 
-    def apply[T](f: F[T]): G[T]
+    def apply1[T](f: F[T]): G[T]
   }
 
   type Id[T] = T
 
-  trait ~>>[G[_]] extends ~>[Id, G] {
-    def apply[T](f: T): G[T]
-  }
+  trait ~>>[G[_]] extends ~>[Id, G]
 
-  type HMap[A <: HList, G[_], M <: ~>>[G]] =
-    A#Foldr[HList, MapHCons[G, M] , HNil.type]
+  type HMap[A <: HList, G[_]] =
+    A#Foldr[HList, MapHCons[G] , HNil.type]
 
-  class MapHCons[G[_], M <: ~>>[G]](implicit m: Manifest[M]) extends Fold[Any, HList] {
-    type Apply[N <: Any, H <: HList] = M#Apply[N] :+: H
+  abstract class MapHCons[G[_]] extends Fold[Any, HList] with ~>>[G]{
+    type Apply[N <: Any, H <: HList] = (Apply1[N] :+: H)
 
     def apply[A, B <: HList](a: A, b: B) = {
-      val t = m.erasure.newInstance.asInstanceOf[M]
-      HCons(t(a), b)
+      HCons(apply1(a), b)
     }
-
   }
-
-  object ToListNT extends ~>>[List] {
-    override def apply[T](f: T): List[T] = List(f)
-  }
-
-  object ToList extends MapHCons[List, ToListNT.type ]
 
   object AppHCons extends Fold[Any, HList] {
     type Apply[N <: Any, H <: HList] = N :+: H
@@ -108,8 +98,9 @@ object HList {
       def reverse_:::[A <: HList](a: A): A Reverse_::: B =
         a.foldl[HList, AppHCons.type, B](AppHCons, b)
 
-      def map[G[_], M <: ~>>[G], T <: MapHCons[G, M]](m: MapHCons[G, M]): HMap[B, G, M] =
-        b.foldr[HList, MapHCons[G, M], HNil.type ](m, HNil)
+      def map[G[_], T <: MapHCons[G]](m: MapHCons[G]): HMap[B, G] =
+        b.foldr[HList, MapHCons[G], HNil.type ](m, HNil)
+
     }
 
   object Length extends Fold[Any, Int] {
@@ -123,12 +114,30 @@ sealed trait HListOps[B <: HList] {
   def :::[A <: HList](a: A): A ::: B
   def reverse: Reverse[B]
   def reverse_:::[A <: HList](a: A): A Reverse_::: B
-  def map[G[_], M <: ~>>[G], T <: MapHCons[G, M]](m: MapHCons[G, M]): HMap[B, G, M]
+  def map[G[_], T <: MapHCons[G]](m: MapHCons[G]): HMap[B, G]
 }
 
 
 
 object HListTest {
+
+  object ToSet extends MapHCons[Set] {
+    override def apply1[T](f: T): Set[T] = Set(f)
+  }
+
+  type Const[C] = {
+    type lambda[T] = C
+  }
+
+  object ToInt extends MapHCons[Const[Int]#lambda] {
+    override def apply1[T](f: T): Const[Int]#lambda[T] = f match {
+      case f: Int => f.toString.length
+      case s: String => s.length
+      case l: List[_] => l.length
+      case d: Double => d.toInt.toString.length
+    }
+  }
+
 
   def main(args: Array[String]): Unit = {
     val t = 1 :+: "hello" :+: 1.01 :+: true :+: HNil
@@ -139,16 +148,16 @@ object HListTest {
       case _ => println("No")
     }
 
-    val a = 3 :+: "ai4" :+: List('r','H') :+: HNil
-    val fat = a ::: t;
+    val a = 1 :+: 3.0 :+: "aiiiii" :+: List('r','H') :+: HNil
+    val fat = a ::: t
 
     val fatr = fat.reverse
 
-    val f = a.map(ToList)
+    val f = a.map[Const[Int]#lambda, ToInt.type ](ToInt)
 
     println(t.tail.head)
     println(fat.tail.tail.tail.tail.head)
     println(fat.length)
-    println(f.head)
+    println(f.tail.tail.head)
   }
 }
